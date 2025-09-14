3864ef2efc0456de39b9744acaf0031af8e3f0ae
Fayez Hesham <fayezhesham510@gmail.com>
2025-09-14 10:10:17 +0300

    update readme

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='cleanframe',
-    version='0.2.0',
+    version='0.2.2',
     author='Fayez Hesham',
     author_email='fayezhesham510@gmail.com',
     description='A Python library for cleaning and validating pandas DataFrames.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/fayezhesham/cleanframe',
+    include_package_data=True,
     packages=find_packages(),
     classifiers=[
         'Programming Language :: Python :: 3',
@@ -23,8 +24,5 @@ setup(
         'pandas>=1.0.0',
     ],
 
-    # package_data={
-    #     'cleanframe': ['data/*.csv'],
-    # },
-    # include_package_data=True, # Set to True to include data files specified in MANIFEST.in
+
 )
